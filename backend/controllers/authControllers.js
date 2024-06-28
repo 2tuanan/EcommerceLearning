3cfeac471b1d44c866904d71b3e032d04d525fa2
tuanan <pantoan288@gmail.com>
2024-06-29 01:14:49 +0700

    Admin Login Page Setup P3

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,7 @@
 const adminModel = require('../models/adminModel')
 const { responseReturn } = require('../utiles/response')
+const bcrpty = require('bcrypt')
+const { createToken } = require('../utiles/tokenCreate')
 
 class authControllers{
     admin_login = async(req,res) => {
@@ -8,7 +10,20 @@ class authControllers{
             const admin = await adminModel.findOne({email}).select('+password')
             // console.log(admin);
             if (admin) {
-                
+                const match = await bcrpty.compare(password, admin.password)
+                // console.log(match);
+                if (match) {
+                    const token = await createToken({
+                        id: admin.id,
+                        role: admin.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires: new Date(Date.now() + 7*24*60*1000)
+                    })
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
             } else {
                 responseReturn(res,404,{error: "Email not found"})
             }
