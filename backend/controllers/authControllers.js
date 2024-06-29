41b61080b9384e65b281691a0a53af0a2128fd4f
tuanan <pantoan288@gmail.com>
2024-06-30 02:28:59 +0700

    Middleware and Routes Manage P2

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -31,6 +31,22 @@ class authControllers{
             responseReturn(res,500,{error: error.message})
         }
     }
+    //End Method
+    getUser = async (req,res) => {
+        const {id,role} = req;
+
+        try {
+            if (role === 'admin') {
+                const user = await adminModel.findById(id)
+                responseReturn(res, 200, {userInfo : user})
+            } else {
+                console.log('Seller Info');
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    //End getUser Method
 }
 
 module.exports = new authControllers()
