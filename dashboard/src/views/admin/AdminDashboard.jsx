f5d943105bac1c8cd808b54131f07575bb97a3ff
tuanan <pantoan288@gmail.com>
2024-07-25 22:22:34 +0700

    Dashboard Design P2(add Chart)

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -2,9 +2,53 @@ import React from 'react';
 import { FaUsers } from 'react-icons/fa';
 import { FaCartShopping } from "react-icons/fa6";
 import { MdCurrencyExchange, MdProductionQuantityLimits } from "react-icons/md";
+import Chart from 'react-apexcharts';
 
 
 const AdminDashboard = () => {
+    const state = {
+        series: [
+            {
+                name: "Orders",
+                data: [23, 34, 45, 56, 76, 34, 23, 76, 78, 88, 78, 80]
+            },
+            {
+                name: "Revenue",
+                data: [67, 39, 45, 56, 90, 34, 23, 76, 78, 88, 89, 90]
+            },
+            {
+                name: "Sellers",
+                data: [34, 39, 56, 56, 80, 34, 23, 76, 99, 90, 98, 88]
+            },
+        ],
+        options: {
+            color: ['#181ee8', '#181ee8'],
+            plotOptions: {
+                radius: 30
+            },
+            chart: {
+                background: 'transparent',
+                foreColor: '#d0d2d6'
+            },
+            dataLabels: {
+                enabled: false
+            },
+            strock: {
+                show: true,
+                curve: ['smooth', 'straight', 'stepline'],
+                lineCap: 'butt',
+                colors: '#f0f0f0',
+                width: .5,
+                dashArray: 0
+            },
+            xaxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            legend: {
+                position: 'top'
+            }
+        }
+    }
     return (
         <div className='px-2 md:px-7 py-5'>
             <div className='w-full grid grid-cols-1 sm:grid-cols-2
@@ -54,6 +98,14 @@ const AdminDashboard = () => {
                     <div className='w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center 
                     items-center text-xl'>
                         <FaCartShopping className='text-[#fae8e8] shadow-lg' />
+                    </div>
+                </div>
+            </div>
+            
+            <div className='w-full flex flex-wrap mt-7'>
+                <div className='w-full lg:w-7/12 lg:pr-3'>
+                    <div className='w-full bg-[#6a5fdf] p-4 rounded-md'>
+                        <Chart options={state.options} series={state.series} type='bar' height={350} />
                     </div>
                 </div>
             </div>
