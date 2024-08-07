9cf0053dedeeeade05d4a1a743f8cd9aaef822e2
tuanan <pantoan288@gmail.com>
2024-08-07 21:40:42 +0700

    Create Pagination P2(Layout)

--- dashboard/src/views/Pagination.jsx
+++ dashboard/src/views/Pagination.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
 const Pagination = ({pageNumber, setPageNumber, totalItem, parPage, showItem}) => {
     let totalPage = Math.ceil(totalItem / parPage)
     let startPage = pageNumber
 
     let dif = totalPage - pageNumber
-    if (div <= showItem) {
+    if (dif <= showItem) {
         startPage = totalPage - showItem
     }
     let endPage = startPage < 0 ? showItem : showItem + startPage
@@ -14,11 +15,30 @@ const Pagination = ({pageNumber, setPageNumber, totalItem, parPage, showItem}) =
         startPage = 1
     }
 
-    const createBton = () => {
-
+    const createBtn = () => {
+        const btns = []
+        for (let i = startPage; i < endPage; i++) {
+            btns.push(
+                <li onClick={()=>setPageNumber(i)} className={`${pageNumber === i 
+                    ? 'bg-indigo-300 shadow-lg shadow-indigo-300/50 text-white'
+                    : 'bg-slate-600 hover:bg-indigo-400 shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]'
+                } w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer`}>
+                    {i}
+                </li>
+            )
+        }
+        return btns
     }
     return (
-        <ul>
+        <ul className='flex gap-3'>
+            {
+                pageNumber > 1 && <li className='w-[33px] h-[33px] rounded-full flex 
+                justify-center items-center bg-slate-300 text-[#000000]'>
+                    <MdKeyboardDoubleArrowLeft />
+                </li>
+            }{
+                createBtn()
+            }
 
         </ul>
     )
