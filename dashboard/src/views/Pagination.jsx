3ac564c534382674a3d21cc4199233edabe271c4
tuanan <pantoan288@gmail.com>
2024-08-07 19:29:35 +0700

    Create Pagination P1

---
+++ dashboard/src/views/Pagination.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Pagination = ({pageNumber, setPageNumber, totalItem, parPage, showItem}) => {
+    let totalPage = Math.ceil(totalItem / parPage)
+    let startPage = pageNumber
+
+    let dif = totalPage - pageNumber
+    if (div <= showItem) {
+        startPage = totalPage - showItem
+    }
+    let endPage = startPage < 0 ? showItem : showItem + startPage
+
+    if (startPage <=0) {
+        startPage = 1
+    }
+
+    const createBton = () => {
+
+    }
+    return (
+        <ul>
+
+        </ul>
+    )
+};
+
+export default Pagination;
