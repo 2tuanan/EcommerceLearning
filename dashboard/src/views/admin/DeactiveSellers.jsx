44e6110595b10dbc33290926d0f48d74ee4953cf
tuanan <pantoan288@gmail.com>
2024-08-27 19:24:37 +0700

    Deactive Sellers Request Page Design P4(Complete)

--- dashboard/src/views/admin/DeactiveSellers.jsx
+++ dashboard/src/views/admin/DeactiveSellers.jsx
@@ -15,7 +15,7 @@ const DeactiveSellers = () => {
         <div className="flex justify-between items-center">
           <select
             onChange={(e) => setParpage(parseInt(e.target.value))}
-            className="px-3 py-2 hover:border-indigo-500
+            className="px-3 py-2 focus:border-indigo-500
                             outline-none bg-[#6a5fdf] border border-slate-700
                         rounded-md text-[#d0d2d6]"
           >
