9b2a4c215a821b69928fbd367785c382158fdcc6
tuanan <pantoan288@gmail.com>
2024-10-23 01:20:05 +0700

    Seller Profile Page Design P2

--- dashboard/src/views/seller/Profile.jsx
+++ dashboard/src/views/seller/Profile.jsx
@@ -4,7 +4,7 @@ import { FadeLoader } from "react-spinners";
 
 const Profile = () => {
     const image = true;
-    const loader = false;
+    const loader = true;
     return (
         <div className='px-2 lg:px-7 py-5'>
             <div className='w-full flex flex-wrap'>
@@ -12,7 +12,19 @@ const Profile = () => {
                     <div className='w-full p-4 bg-[#6a5fdf] rounded-md text-[#d0d2d6]'>
                         <div className='flex justify-center items-center py-3'>
                             {
-                                image ? <label htmlFor=""></label> : <label className='flex justify-center items-center flex-col 
+                                image ? <label htmlFor="img" className='h-[150px] w-[200px] relative p-3 cursor-pointer
+                                overflow-hidden'>
+                                    <img src="http://localhost:3000/images/demo.jpg" alt="" />
+                                    {
+                                        !loader && <div className='absolute top-0 left-0 w-full h-full 
+                                        bg-slate-600 opacity-70 flex justify-center items-center
+                                        z-20'>
+                                            <span>
+                                                <FadeLoader color={'#d0d2d6'} loading={loader} size={50} />
+                                            </span>
+                                        </div> 
+                                    }
+                                </label> : <label className='flex justify-center items-center flex-col 
                                 h-[150px] w-[200px] cursor-pointer border border-dashed hover:border-red-500 
                                 border-[#d0d2d6] relative' 
                                 htmlFor='img'>
@@ -29,6 +41,7 @@ const Profile = () => {
                                     }
                                 </label>
                             }
+                            <input type="file" className='hidden' id='img' />
                         </div>
                     </div>
                 </div>
