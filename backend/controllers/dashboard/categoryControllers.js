780dcc6b8c54ee0585441fb09bfba86cfbdd6057
tuanan <pantoan288@gmail.com>
2024-11-22 02:51:35 +0700

    Setup Category Add And Get P4

--- backend/controllers/dashboard/categoryControllers.js
+++ backend/controllers/dashboard/categoryControllers.js
@@ -1,7 +1,20 @@
-
+const formidable = require("formidable");
+const { responseReturn } = require("../../utiles/response");
 class categoryControllers {
     add_category = async (req, res) => {
-        console.log(req.body);
+        const form = formidable();
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                responseReturn(res, 404, { error: 'Something went wrong' });
+            } else {
+                console.log(fields);
+                console.log(files);
+                name = name.trim();
+                const slug = name.split(' ').join('-');
+            }
+        })    
+    }
+    get_category = async (req, res) => {
         
     }
 }
