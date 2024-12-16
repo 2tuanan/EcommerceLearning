1a777645a050c436b21035545f398f22367ffc00
tuanan <pantoan288@gmail.com>
2024-12-16 22:01:37 +0700

    Setup Category Add And Get P8

--- backend/controllers/dashboard/categoryControllers.js
+++ backend/controllers/dashboard/categoryControllers.js
@@ -42,10 +42,30 @@ class categoryControllers {
             }
         })    
     }
+    // End method
     get_category = async (req, res) => {
         const {page, searchValue, parPage} = req.query;
+        const skipPage = parseInt(parPage) * (parseInt(page) - 1);
         
+        try {
+            if (searchValue) {
+                const categorys = await categoryModel.find({
+                    $text: { $search: searchValue }
+                }).skip(skipPage).limit(parPage).sort({ createdAt: -1 });
+                const totalCategory = await categoryModel.find({
+                    $text: { $search: searchValue }
+                }).countDocuments();
+                responseReturn(res, 200, { categorys, totalCategory });
+            } else {
+                const categorys = await categoryModel.find().skip(skipPage).limit(parPage).sort({ createdAt: -1 });
+                const totalCategory = await categoryModel.find().countDocuments();
+                responseReturn(res, 200, { categorys, totalCategory });
+            }
+        } catch (error) {
+            
+        }
     }
+    // End method
 }
 
 module.exports = new categoryControllers();
