fb773a8bf610990a5e6616ce513bcebdcfb28733
tuanan <pantoan288@gmail.com>
2024-12-20 02:30:02 +0700

    Add Product Setup P6

--- backend/controllers/dashboard/productController.js
+++ backend/controllers/dashboard/productController.js
@@ -1,15 +1,41 @@
 const formidable = require("formidable");
 const { responseReturn } = require("../../utiles/response");
 const cloudinary = require('cloudinary').v2;
+const productModel = require('../../models/productModel');
 
 class productController {
     add_product = async (req, res) => {
         const form = formidable({ multiples: true });
         form.parse(req, async (err, fields, files) => {
-            console.log(files.images[0]);
-            console.log(fields);
+            let {name, price, description, category, stock, discount, shopName, brand} = fields
+            const {images} = files;
+            name = name.trim();
+            const slug = name.split(' ').join('-');
+            
+            cloudinary.config({
+                cloud_name: process.env.cloud_name,
+                api_key: process.env.api_key,
+                api_secret: process.env.api_secret,
+                secure: true
+            })
+
+            try {
+                let allImageUrl = [];
+                for (let i = 0; i < images.length; i++) {
+                    const result = await cloudinary.uploader.upload(images[i].filepath, {folder: 'products'})
+                    allImageUrl = [...allImageUrl, result.url]
+                }
+
+                await productModel.create({
+                    
+                })
+            } catch (error) {
+                
+            }
         })
     }
+    // End method
+
 }
 
 module.exports = new productController();
